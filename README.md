679d3b6fb98281fa9968863ce4ad6a35a30a2ad9
Rye <rye@alchemyviewer.org>
2025-10-15 09:50:54 -0400

    Update build files for SDL3

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-# 3p-SDL
-
-This is a fork of [Alchemy's](https://www.alchemyviewer.org/) [3p-SDL2](https://git.alchemyviewer.org/alchemy/thirdparty/3p-sdl2)
-repository configured to build and package using [autobuild](https://wiki.secondlife.com/wiki/Autobuild).
+# 3p-SDL3
 
 [Simple DirectMedia Layer](SDL/docs/README.md) is a cross-platform development library designed
 to provide low level access to audio, keyboard, mouse, joystick, and graphics
@@ -11,9 +8,10 @@ hardware via OpenGL and Direct3D.
 
 * Install [autobuild](https://wiki.secondlife.com/wiki/Autobuild) utility.
 * Invoke autobuild at the command line:
-<pre>
+
+```      
         autobuild build
         autobuild package
-</pre>
 
-That will produce a bundled asset of SDL2 libs and header files called something like: `SDL2-2.24.1-linux-012345678.tar.bz2`
+```
+That will produce a bundled asset of SDL3 libs and header files called something like: `SDL3-3.22.0-linux-012345678.tar.bz2`
--- build-cmd.sh
+++ build-cmd.sh
@@ -19,7 +19,7 @@ fi
 
 TOP="$(dirname "$0")"
 
-SDL_SOURCE_DIR="SDL2"
+SDL_SOURCE_DIR="SDL3"
 
 stage="$(pwd)"
 
@@ -28,7 +28,7 @@ source_environment_tempfile="$stage/source_environment.sh"
 "$autobuild" source_environment > "$source_environment_tempfile"
 . "$source_environment_tempfile"
 
-# remove_cxxstd
+# remove_cxxstd apply_patch
 source "$(dirname "$AUTOBUILD_VARIABLES_FILE")/functions"
 
 # Restore all .sos
@@ -47,13 +47,13 @@ case "$AUTOBUILD_PLATFORM" in
     windows*)
         load_vsvars
 
-        mkdir -p "$stage/include/SDL2"
+        mkdir -p "$stage/include/SDL3"
         mkdir -p "$stage/lib/debug"
         mkdir -p "$stage/lib/release"
 
         mkdir -p "build_debug"
         pushd "build_debug"
-            cmake .. -G "$AUTOBUILD_WIN_CMAKE_GEN" -A "$AUTOBUILD_WIN_VSPLATFORM" -DCMAKE_INSTALL_PREFIX=$(cygpath -m $stage)/debug
+            cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$(cygpath -m $stage)/debug
 
             cmake --build . --config Debug
             cmake --install . --config Debug
@@ -69,7 +69,7 @@ case "$AUTOBUILD_PLATFORM" in
 
         mkdir -p "build_release"
         pushd "build_release"
-            cmake .. -G "$AUTOBUILD_WIN_CMAKE_GEN" -A "$AUTOBUILD_WIN_VSPLATFORM" -DCMAKE_INSTALL_PREFIX=$(cygpath -m $stage)/release
+            cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$(cygpath -m $stage)/release
 
             cmake --build . --config Release
             cmake --install . --config Release
@@ -81,7 +81,7 @@ case "$AUTOBUILD_PLATFORM" in
 
             cp $stage/release/bin/*.dll $stage/lib/release/
             cp $stage/release/lib/*.lib $stage/lib/release/
-            cp $stage/release/include/SDL2/*.h $stage/include/SDL2/
+            cp $stage/release/include/SDL3/*.h $stage/include/SDL3/
         popd
     ;;
     darwin*)
@@ -93,19 +93,29 @@ case "$AUTOBUILD_PLATFORM" in
             cc_opts="$(remove_cxxstd $opts)"
             ld_opts="$ARCH_ARGS"
 
+            if [ "$arch" = "x86_64" ] ; then
+                SDL_CPU_X64="1"
+                SDL_CPU_ARM64="0"
+            else
+                SDL_CPU_X64="0"
+                SDL_CPU_ARM64="1"
+            fi
+
             mkdir -p "build_$arch"
             pushd "build_$arch"
                 CFLAGS="$cc_opts" \
                 CXXFLAGS="$opts" \
                 LDFLAGS="$ld_opts" \
-                cmake .. -GNinja -DCMAKE_BUILD_TYPE="Release" \
+                cmake .. -G Ninja -DCMAKE_BUILD_TYPE="Release" -DCMAKE_CONFIGURATION_TYPES="Release" \
                     -DCMAKE_C_FLAGS="$cc_opts" \
                     -DCMAKE_CXX_FLAGS="$opts" \
                     -DCMAKE_OSX_ARCHITECTURES:STRING="$arch" \
                     -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
                     -DCMAKE_MACOSX_RPATH=YES \
                     -DCMAKE_INSTALL_PREFIX="$stage" \
-                    -DCMAKE_INSTALL_LIBDIR="$stage/lib/release/$arch"
+                    -DCMAKE_INSTALL_LIBDIR="$stage/lib/release/$arch" \
+                    -DSDL_CPU_ARM64="$SDL_CPU_ARM64" \
+                    -DSDL_CPU_X64="$SDL_CPU_X64"
 
                 cmake --build . --config Release
                 cmake --install . --config Release
@@ -114,21 +124,20 @@ case "$AUTOBUILD_PLATFORM" in
 
 
         # create universal libraries
-        lipo -create -output ${stage}/lib/release/libSDL2.dylib ${stage}/lib/release/x86_64/libSDL2.dylib ${stage}/lib/release/arm64/libSDL2.dylib
-        lipo -create -output ${stage}/lib/release/libSDL2_test.a ${stage}/lib/release/x86_64/libSDL2_test.a ${stage}/lib/release/arm64/libSDL2_test.a
-        lipo -create -output ${stage}/lib/release/libSDL2main.a ${stage}/lib/release/x86_64/libSDL2main.a ${stage}/lib/release/arm64/libSDL2main.a
+        lipo -create -output ${stage}/lib/release/libSDL3.dylib ${stage}/lib/release/x86_64/libSDL3.dylib ${stage}/lib/release/arm64/libSDL3.dylib
+        lipo -create -output ${stage}/lib/release/libSDL3_test.a ${stage}/lib/release/x86_64/libSDL3_test.a ${stage}/lib/release/arm64/libSDL3_test.a
 
         pushd "${stage}/lib/release"
-            install_name_tool -id "@rpath/libSDL2.dylib" "libSDL2.dylib"
-            dsymutil libSDL2.dylib
-            strip -x -S libSDL2.dylib
+            install_name_tool -id "@rpath/libSDL3.dylib" "libSDL3.dylib"
+            dsymutil libSDL3.dylib
+            strip -x -S libSDL3.dylib
         popd
         ;;
     linux*)
         # Default target per autobuild build --address-size
         opts="${TARGET_OPTS:--m$AUTOBUILD_ADDRSIZE $LL_BUILD_RELEASE}"
 
-        mkdir -p "$stage/include/SDL2"
+        mkdir -p "$stage/include/SDL3"
         mkdir -p "$stage/lib/release"
 
         PREFIX_RELEASE="$stage/temp_release"
@@ -145,9 +154,8 @@ case "$AUTOBUILD_PLATFORM" in
             cmake --install . --config Release
         popd
 
-        cp -a $PREFIX_RELEASE/include/SDL2/*.* $stage/include/SDL2
+        cp -a $PREFIX_RELEASE/include/SDL3/*.* $stage/include/SDL3
         cp -a $PREFIX_RELEASE/lib/*.so* $stage/lib/release
-        cp -a $PREFIX_RELEASE/lib/libSDL2main.a $stage/lib/release
     ;;
 
     *)
@@ -157,4 +165,4 @@ esac
 popd
 
 mkdir -p "$stage/LICENSES"
-cp "$TOP/$SDL_SOURCE_DIR/LICENSE.txt" "$stage/LICENSES/SDL2.txt"
+cp "$TOP/$SDL_SOURCE_DIR/LICENSE.txt" "$stage/LICENSES/SDL3.txt"
